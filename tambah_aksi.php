cdf1b2dc0d538906b4d8c8e4e296c4fa232ce469
2023070865 <2023070865@student.pppkpetra.sch.id>
2025-11-29 18:45:12 +0700

    Update tambah_aksi.php

--- tambah_aksi.php
+++ tambah_aksi.php
@@ -1,6 +1,6 @@
 <?php
 include "koneksi.php";
-
+$KM = $_POST['nama_lengkap'];
 $NM = $_POST['umur'];
 $Jurusan = $_POST['jurusan'];
 $Alamat = $_POST['alamat'];
@@ -9,5 +9,6 @@ $NIK = $_POST['nik'];
 mysqli_query($koneksi, "INSERT INTO aplication (nama_lengkap, umur, jurusan, alamat, nik) values('$KM','$NM','$Jurusan','$Alamat','$NIK')");
 
 header("location:index.php");
+
 
 ?>
